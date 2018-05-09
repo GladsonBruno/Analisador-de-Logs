e911bc3f70b1d4cf1ce8520563240afceec247e2
GladsonBruno <gladsonbruno16@gmail.com>
2018-05-09 16:00:03 -0300

    Pagina inicial em andamento

---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+const electron_remote = require('electron').remote;
+const boxDialog = electron_remote.dialog;
+
+$(document).ready(function(){
+
+    ShowLoader();
+
+});
+
+function ShowLoader(){
+    var loader = `<div class='container center'>
+    <div class="preloader-wrapper big active">
+        <div class="spinner-layer spinner-blue-only">
+            <div class="circle-clipper left">
+                <div class="circle"></div>
+            </div>
+            <div class="gap-patch">
+                <div class="circle"></div>
+            </div>
+            <div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+        </div>
+    </div>
+    </div>
+    `;
+
+    M.toast({
+        html: loader,
+        timeRemaining: 200,
+        displayLength: 1000,
+        classes: 'container center transparent'
+    });
+}
+
+function CarregarLOG(){
+    const opcoesCertificado = {
+        properties: ['openFile'],
+        filters: [{
+            name: "LOG File",
+            extensions: ['log']
+        }]
+    };
+
+    boxDialog.showOpenDialog(opcoesCertificado, (file) => {
+        $("#caminho-log").val(file);
+    });
+}
+
+function RedirecionaPaginaAnalisar(){
+    if($("#caminho-log").val() == ""){
+        alert("Insira o caminho do log");
+    } else {
+        localStorage.setItem("CaminhoLog", $("#caminho-log").val());
+        ShowLoader();
+        setInterval(()=> {
+            window.location.assign("../pages/RetornoPesquisa.html");
+        }, 1200);   
+    }
+}
