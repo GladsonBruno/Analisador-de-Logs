ba0514a6b3cf1a0010641ebe111c762c1a350b40
GladsonBruno <gladsonbruno16@gmail.com>
2018-05-24 12:37:20 -0300

    Forma de pesquisa do ciclo de vida alterada para buscar no XML

--- scripts/analisador.js
+++ scripts/analisador.js
@@ -7,9 +7,27 @@ class Evento_S {
         this.ID = id;
         this.Status = undefined;
         this.CicloDeVida = {
-            "Ocorrencias": []
+            "Ocorrencias": [],
+            "DataHora": []
         };
-    }
+        this.GetDataHora = function(EXP, Conteudo){
+            let data_hora = /(^\d{2})\-(\d{2})\-(\w{4})\s(\d{2}):(\d{2}):(\d{2})\.\d{3}.*/;
+            
+            let dataOcorrencia = {
+                "Data": {
+                    "Ano": data_hora.exec(Conteudo)[3],
+                    "Mes": data_hora.exec(Conteudo)[2],
+                    "Dia": data_hora.exec(Conteudo)[1]
+                },
+                "Hora": data_hora.exec(Conteudo)[4],
+                "Minuto": data_hora.exec(Conteudo)[5],
+                "Segundos": data_hora.exec(Conteudo)[6],
+                "HoraCompleta": data_hora.exec(Conteudo)[4] + ":" + data_hora.exec(Conteudo)[5] + ":" + data_hora.exec(Conteudo)[6]
+            };
+        
+            return dataOcorrencia;
+        }
+    };
 }
 
 function PesquisarPorID(){
@@ -41,7 +59,14 @@ function PesquisarPorID(){
     let DescricaoUmaLinha = /^.*\<(descricao)\>(.*)\<\/\1\>.*/gm;
     let FimRetornoID = /^.*\<\/evento\>/gm
     let InicioBuscaDescricao = false;
-    
+    let Processamento_Consulta = new RegExp("^.*(Processamento da consulta do evento ID: " + id + ".*).*", "gm");
+    //Verifica se a linha não tem tags xml
+    let NaoXML = /^.*(\<)|(\>).*/gm;
+    let FalhaMontagemXML = new RegExp("^.*ID:(" + id + " apresentou problemas na montagem do XML.*)", "gm");
+
+    //Pegar data e hora do processamento
+    // /^.*<(dhProcessamento>)(\d{4})-(\d{2})-(\d{2})T(.{8}).*\<\/\1/
+
     let IniciarBuscaRetorno = false;
 
     let indexOcorrencia = 0;
@@ -65,35 +90,84 @@ function PesquisarPorID(){
     })
     .on('readable', function () { this.readLine(); })
     .on('read', function (line, count) {
+        //Verifica status Extraido para processamento
         if(line.match(ExtraidoProcessamento) != null){
+
             Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push("ID atualizado na base de dados para Extraído para processamento");
+            Evento.CicloDeVida.DataHora.push(Evento.GetDataHora(ExtraidoProcessamento, line));
+
+        //Verifica status Enviado
         } else if(line.match(ID_Enviado) != null){
+
             Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push("ID Sendo enviado");
-        } else if(line.match(RetornoID) != null){
-            IniciarBuscaRetorno = true;
-        } else if( line.match(FimRetornoID) != null && IniciarBuscaRetorno == true){
-            IniciarBuscaRetorno = false;
+            Evento.CicloDeVida.DataHora.push(Evento.GetDataHora(ExtraidoProcessamento, line));
             indexOcorrencia++;
+            Evento.CicloDeVida.Ocorrencias.push({"Ocorrencia": [] })
+
+        //Verifica se a linha é o início do XML com o processamento do evento
+        } else if(line.match(RetornoID) != null){
+
+            IniciarBuscaRetorno = true;
             Evento.CicloDeVida.Ocorrencias.push({"Ocorrencia": []})
-        } else if(IniciarBuscaRetorno == true && line.match(CodigoResposta) != null){
-            var resposta = CodigoResposta.exec(line)[2];
+
+        //Verifica se é o Fim do retorno do evento no XML
+        } else if( line.match(FimRetornoID) != null && IniciarBuscaRetorno == true){
+
+            IniciarBuscaRetorno = false;
+            
+
+        // IniciarBuscaRetorno = Verifica se está entre as linhas com o retorno do XML
+        //Verifica o Código de Resposta do evento
+        } else if( Processamento_Consulta.test(line) == true ){
+
+            Evento.CicloDeVida.DataHora.push(Evento.GetDataHora(Processamento_Consulta, line));
+            indexOcorrencia++;
+
+        } else if(IniciarBuscaRetorno == true && line.search(CodigoResposta) != -1 ){
+
+            var resposta;
+            try {
+                resposta = CodigoResposta.exec(line)[2];
+            } catch(e) {
+                resposta = CodigoResposta.exec(line)[2];
+            }
+
             Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push("Código de Resposta: " + resposta);
+
+        // IniciarBuscaRetorno = Verifica se está entre as linhas com o retorno do XML
+        //Verifica a descrição da resposta do evento
         } else if(IniciarBuscaRetorno == true && line.match(DescricaoResposta) != null){
+
             var descResposta = DescricaoResposta.exec(line)[2];
             Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push("Descrição Resposta: " + descResposta);
+
+        // IniciarBuscaRetorno = Verifica se está entre as linhas com o retorno do XML
+        //Verifica o tipo de ocorrência do evento
         } else if (IniciarBuscaRetorno == true && line.match(TipoOcorrencia) != null){
+            
             var tipo = TipoOcorrencia.exec(line)[2];
             Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push("Ocorrência tipo: " + tipo);
+
+        // IniciarBuscaRetorno = Verifica se está entre as linhas com o retorno do XML
+        // Verifica o código de retorno da Ocorrência.
         } else if( IniciarBuscaRetorno == true && line.match(CodigoOcorrencia) != null){
+
             var codigo = CodigoOcorrencia.exec(line)[2];
             Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push("Código Ocorrência: " + codigo);
+
+        // IniciarBuscaRetorno = Verifica se está entre as linhas com o retorno do XML
+        //Verifica se a linha analisada é a linha do xml que mostra a descrição da ocorrencia do evento.
         } else if( IniciarBuscaRetorno == true && line.match(InicioDescricao) != null){
+
             var descricao = InicioDescricao.exec(line)[1];
             Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push("Descrição Ocorrência: " + descricao);
             InicioBuscaDescricao = true;
+
+        // IniciarBuscaRetorno = Verifica se está entre as linhas com o retorno do XML
+        // InicioBuscaDescricao Verifica se a linha analisada é a linha do xml que mostra a descrição da ocorrencia do evento.
         } else if( IniciarBuscaRetorno == true && InicioBuscaDescricao == true ){
-            //Linha repetida pois o match nem sempre retorna o esperado
-            if( line.match(FimRetornoID) != null ){
+
+            if( line.search(FimDescricao) != -1 ){
                 var fimDesc;
                 try{
                     fimDesc = FimDescricao.exec(line)[1];
@@ -104,24 +178,57 @@ function PesquisarPorID(){
                     }
                 }
                 InicioBuscaDescricao = false;
-            } else {
-                var fimDesc;
-                try{
-                    fimDesc = FimDescricao.exec(line)[1];
-                } catch(e){
-                    if(fimDesc == null){
-                        fimDesc = FimDescricao.exec(line)[1];
-                        Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push(fimDesc);
-                    }
-                }
-                InicioBuscaDescricao = false;
+            //Verifica se é uma linha de descrição que não contem tags xml.
+            } else if( line.match(NaoXML) == null ){
+                Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push(line);
+            } 
+        //a função search retorna 0 caso encontre correspondencia, caso contrario retorna -1
+        } else if( line.search(FalhaMontagemXML) == 0 ){
+            var retornoFalhaXML;
+            //Exec retorna positivo e negativo para a mesma busca.
+            try {
+                retornoFalhaXML = FalhaMontagemXML.exec(line)[1];
+            } catch(e){
+                retornoFalhaXML = FalhaMontagemXML.exec(line)[1];
             }
+            Evento.CicloDeVida.Ocorrencias[indexOcorrencia].Ocorrencia.push(retornoFalhaXML);
+            Evento.CicloDeVida.DataHora.push(Evento.GetDataHora(FalhaMontagemXML, line));
+            indexOcorrencia++;
+            Evento.CicloDeVida.Ocorrencias.push({"Ocorrencia": [] })
         }
     })
     .on('end', function (done) {
-        Evento.CicloDeVida.Ocorrencias.forEach(val => {
-            console.log(val);
-        });
-        console.log(Evento.CicloDeVida.Ocorrencias.length)
+        if(Evento.CicloDeVida.Ocorrencias.length > 0){
+            
+            //Mostrando div que contem o timeline
+            if($(".page").hasClass("hide") == true){
+                $(".page").removeClass("hide");
+            }
+
+            for(i = 0; i < Evento.CicloDeVida.Ocorrencias.length - 1; i++){
+                let hora = Evento.CicloDeVida.DataHora[i].HoraCompleta;
+                ocorrencia = `
+                    <div class="timeline__box">
+                        <div class="timeline__date">
+                            <span class="timeline__hora"> Ocorrência </span>
+                            <span class="timeline__hora"> `+ hora +` </span>
+                        </div>
+                        <div class="timeline__post">
+                        <div class="timeline__content" id="EventoNegocio01_`+ i +`">
+                        </div>
+                        </div>
+                    </div>
+                    `;
+                $(".conteudo").append(ocorrencia);
+
+                for(var j = 0; j < Evento.CicloDeVida.Ocorrencias[i].Ocorrencia.length; j++){
+                    $("#EventoNegocio01_" + i).append("<p>" + Evento.CicloDeVida.Ocorrencias[i].Ocorrencia[j] + "</p>");
+                }
+            }
+            
+            $(".timeline__year").html("").html(id);
+        }
+        
+        console.log(Evento.CicloDeVida)
     });
 }
